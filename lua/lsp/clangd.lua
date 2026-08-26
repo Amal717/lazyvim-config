3ca7c11bbeb46d0e27ae8eaa18f364bf9766df9f
Amal Thomas <thomasamal43@gmail.com>
2026-08-26 12:48:32 +0530

    updated clangd and options

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -4,6 +4,7 @@ return {
         "--background-index",
         "--clang-tidy",
         "--header-insertion=never",
+        "--query-driver=/usr/bin/arm-none-eabi-gcc",
     },
     init_options = {
         clangdFileStatus = true,
